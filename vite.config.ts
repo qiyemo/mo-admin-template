3e73b58724aa7320ec34428c9f7c8ddcdec8b0ab
moqiye <qiye.mo@foxmail.com>
2023-01-09 15:11:02 +0800

    feat: first commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/*
+ * @Author: qiye
+ * @LastEditors: qiye
+ * @description: page description
+ * @Date: 2023-01-09 13:53:38
+ * @LastEditTime: 2023-01-09 13:55:26
+ */
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  plugins: [vue()],
+})
