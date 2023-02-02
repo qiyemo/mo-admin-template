07a71e1d466f1c87de1a16540ea67e7c15d4b3ad
moqiye <qiye.mo@foxmail.com>
2023-02-02 16:34:49 +0800

    fix:修改配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,29 @@
  * @Date: 2023-01-09 13:53:38
  * @LastEditTime: 2023-01-09 13:55:26
  */
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
 
 import Components from 'unplugin-vue-components/vite';
-import {ElementPlusResolver} from 'unplugin-vue-components/resolvers';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-
-
-export default async ({mode, command}) => {
-  return defineConfig({
-    plugins: [
-      vue(),
-      eslintPlugin({
-        include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
-      }),
-      Components({
-        resolvers: [ElementPlusResolver()]
-      })
-    ],
-    resolve: {
-      // 配置路径别名
-      alias: {
-        '@': '/src',
-      }
-    }
-  });
-}
+export default async ({ mode, command }) => {
+	return defineConfig({
+		plugins: [
+			vue(),
+			eslintPlugin({
+				include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+			}),
+			Components({
+				resolvers: [ElementPlusResolver()],
+			}),
+		],
+		resolve: {
+			// 配置路径别名
+			alias: {
+				'@': '/src',
+			},
+		},
+	});
+};
